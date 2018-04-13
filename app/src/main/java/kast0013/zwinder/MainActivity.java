18dde66c9a30eccc0148a50e93ea6e7620aa8580
karim stock <karim.h.stock@gmail.com>
2018-04-13 02:09:36 +0200

    Changed Background colors for MainActivity & SwipeCard items +Added signout button

--- app/src/main/java/kast0013/zwinder/MainActivity.java
+++ app/src/main/java/kast0013/zwinder/MainActivity.java
@@ -1,16 +1,28 @@
 package kast0013.zwinder;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.lorentzos.flingswipe.SwipeFlingAdapterView;
 
 import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends Activity {
 
@@ -18,21 +30,30 @@ public class MainActivity extends Activity {
     private ArrayAdapter<String> arrayAdapter;
     private int i;
 
+    private FirebaseAuth firebaseAuth;
+
+    @BindView(R.id.btn_logout) Button _logoutButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        _logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.signOut();
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
         al = new ArrayList<>();
-        al.add("php");
-        al.add("c");
-        al.add("python");
-        al.add("java");
-        al.add("html");
-        al.add("c++");
-        al.add("css");
-        al.add("javascript");
-
+        al.add("Names");
         arrayAdapter = new ArrayAdapter<>(this, R.layout.item, R.id.helloText, al);
 
         SwipeFlingAdapterView flingContainer = findViewById(R.id.frame);
@@ -63,7 +84,8 @@ public class MainActivity extends Activity {
             @Override
             public void onAdapterAboutToEmpty(int itemsInAdapter) {
                 // Ask for more data here
-                al.add("XML ".concat(String.valueOf(i)));
+                //al.add("XML ".concat(String.valueOf(i)));
+                checkUser();
                 arrayAdapter.notifyDataSetChanged();
                 Log.d("LIST", "notified");
                 i++;
@@ -74,11 +96,42 @@ public class MainActivity extends Activity {
             }
 
         });
+
+
         // Optionally add an OnItemClickListener
         flingContainer.setOnItemClickListener(new SwipeFlingAdapterView.OnItemClickListener() {
             @Override
             public void onItemClicked(int itemPosition, Object dataObject) {
                 Toast.makeText(MainActivity.this, "CLICKED!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    public void checkUser(){
+        DatabaseReference maleUserDb = FirebaseDatabase.getInstance().getReference().child("Users").child("Male");
+        maleUserDb.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                if(dataSnapshot.exists()) al.add(dataSnapshot.child("name").getValue().toString());
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
             }
         });
     }
